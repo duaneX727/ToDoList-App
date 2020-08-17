8e7d53bdd6222f13a827d21f14a146d881ff7fe2
duaneX727 <duaneX727@users.noreply.github.com>
2020-08-17 14:20:38 -0400

    Add files via upload

---
+++ precache-manifest.600eefb3273cfc69b4da9652356cd2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0596a3075eb8a4374cd649022a94433f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f4a8b9a62b6b782504",
+    "url": "/static/css/main.fb140e4b.chunk.css"
+  },
+  {
+    "revision": "4c3a17e3a76e5d0db859",
+    "url": "/static/js/2.314981d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.314981d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f4a8b9a62b6b782504",
+    "url": "/static/js/main.ed19820e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
